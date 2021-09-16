0bc7cbf6d501afd3893884cf165793394ffda0e0
Aldo Ojeda <aldo.l.ojeda.l@gmail.com>
2021-09-16 17:15:10 +0200

    Djangotestfront done

--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -1,40 +1,77 @@
 <template>
-    <div class="page-my-account">
-        <div class="columns is-multiline">
-            <div class="column is-12">
-                <h1 class="title">
-                    My account
-                </h1>
-            </div>
+  <div class="page-my-account">
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h1 class="title">My account</h1>
+      </div>
 
-            <div class="column is-12">
-                <button @click="logOut()" class="button is-danger">
-                    Log out
-                </button>
-            </div>
-        </div>
+      <div class="column is-12">
+        <button @click="logOut()" class="button is-danger">Log out</button>
+      </div>
+
+      <hr />
+
+      <div class="column is-12">
+        <h2 class="subtitle">My orders</h2>
+
+        <OrderSummary
+          v-for="order in orders"
+          v-bind:key="order.id"
+          v-bind:order="order"
+        />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
+import OrderSummary from "../components/OrderSummary.vue";
 export default {
-    name: 'MyAccount',
-    methods: {
-        logOut() {
-            axios.defaults.headers.common['Authorization'] = ''
+  name: "MyAccount",
+  components: {
+    OrderSummary,
+  },
+  data() {
+    return {
+      orders: [],
+    };
+  },
+  mounted() {
+    document.title = "My account | Djackets";
 
-            localStorage.removeItem('token')
-            localStorage.removeItem('username')
-            localStorage.removeItem('userid')
+    this.getMyOrders();
+  },
+  methods: {
+    logOut() {
+      axios.defaults.headers.common["Authorization"] = "";
 
-            this.$store.commit('removeToken')
+      localStorage.removeItem("token");
+      localStorage.removeItem("username");
+      localStorage.removeItem("userid");
 
-            this.$router.push('/')
-        }
+      this.$store.commit("removeToken");
+
+      this.$router.push("/");
     },
-    data() {
-        return {}
-    }
-}
+    async getMyOrders() {
+      this.$store.commit("setIsLoading", true);
+
+      const headers = {
+        headers: { Authorization: `Token ${this.$store.state.token}` },
+      };
+
+      await axios
+        .get("/api/v1/orders/", headers)
+        .then((response) => {
+          this.orders = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      this.$store.commit("setIsLoading", false);
+    },
+  },
+};
 </script>
